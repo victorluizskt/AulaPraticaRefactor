11d9961ee8de2bf40ad60c1eb7808c9efdd703ce
Victor Luiz <victorluizcefet@gmail.com>
2022-09-14 11:17:09 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -25,7 +25,7 @@ public class Customer {
      
      while (rentals.hasMoreElements()) {
         Rental each = (Rental) rentals.nextElement();
-        double thisAmount = amountFor(each);
+        double thisAmount = each.getCharge();
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
@@ -43,27 +43,5 @@ public class Customer {
      result += "You earned " + String.valueOf(frequentRenterPoints) +
              " frequent renter points";
      return result;
-   }
-
-   private double amountFor(Rental aRental) {
-      //determine amounts for each line
-        double thisAmount = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (aRental.getDaysRented() > 2)
-                 thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += aRental.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (aRental.getDaysRented() > 3)
-                 thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-               break;
-        }
-
-      return thisAmount;
    }
 }
