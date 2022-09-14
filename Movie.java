9ee6a86720f761841596ed8a0e3bea92460a9ece
Victor Luiz <victorluizcefet@gmail.com>
2022-09-14 11:40:44 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -32,29 +32,12 @@ public class Movie {
       }
    }
 
-  public String getTitle (){
+    public String getTitle (){
       return _title;
-  }
+    } 
 
-  public double getCharge(int daysRented){
-    double thisAmount = 0;
-    switch (getPriceCode()) {
-        case Movie.REGULAR:
-            thisAmount += 2;
-            if (daysRented > 2)
-                thisAmount += (getPriceCode() - 2) * 1.5;
-            break;
-        case Movie.NEW_RELEASE:
-            thisAmount += getPriceCode() * 3;
-            break;
-        case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (daysRented > 3)
-                thisAmount += (getPriceCode() - 3) * 1.5;
-            break;
-    }
-
-        return thisAmount;
+   public double getCharge(int daysRented) {
+    return _price.getCharge(daysRented);
    }
 
    public int getFrequentRenterPoints(int daysRented){
